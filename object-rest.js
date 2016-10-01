ebce850739bb6516915127a87cc578c8ef8f30e8
Christophe Hurpeau <christophe@hurpeau.com>
2016-10-01 16:19:16 +0200

    add modules-commonjs and use loose mode

--- object-rest.js
+++ object-rest.js
@@ -1,7 +1,7 @@
 // https://phabricator.babeljs.io/T7086
 module.exports = {
   plugins: [
-    require('babel-plugin-transform-es2015-destructuring'),
+    [require('babel-plugin-transform-es2015-destructuring'), { loose: true }],
     require('babel-plugin-transform-es2015-parameters'),
   ],
 };
