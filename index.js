630573971500e1a2c567a820354ffafa3b3102a7
Christophe Hurpeau <christophe@hurpeau.com>
2017-08-11 00:21:37 +0200

    fix: use noInterop

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
     plugins: [
-        [require('babel-plugin-transform-es2015-modules-commonjs'), { loose: true }]
+        [require('babel-plugin-transform-es2015-modules-commonjs'), { loose: true, noInterop: true }]
     ]
 };
